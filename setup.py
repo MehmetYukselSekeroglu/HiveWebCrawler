1f524ea25d2ba151dd18c48e3dc7b5a9926d3568
MehmetYukselSekeroglu <dijital_evren@protonmail.com>
2024-04-15 13:52:40 +0300

    v0.0.2 setup.py update

--- setup.py
+++ setup.py
@@ -1,13 +1,24 @@
 import setuptools
 
+long_describe = ""
+long_describe_format = "text/markdown"
+
+with open("README.md","r") as readmeFile:
+    long_describe = readmeFile.read()
 
 setuptools.setup(
     name="HiveWebCrawler",
     author="MehmetYukselSekeroglu",
-    version="0.0.1",
+    version="0.0.2",
     author_email="dijital_evren@protonmail.com",
     description="Simple Python 3.x Web Crawler, Images, Urls, Emails, Phone numbers",
-    long_description=None,
+    long_description=long_describe,
+    long_descriptlion_content_type=long_describe_format,
+    keywords="PyPi, Web Crawler, Web Image Crawler, Web URL Crawler, Web Email Crawler, Web Phone Number Crawler",
+    project_urls={
+        "Bug Reports":"https://github.com/MehmetYukselSekeroglu/HiveWebCrawler/issues",
+        "Source Code":"https://github.com/MehmetYukselSekeroglu/HiveWebCrawler"
+    },
     url="https://github.com/MehmetYukselSekeroglu/HiveWebCrawler",
     package_dir={"":"HiveWebCrawler"},
     packages=setuptools.find_packages(where="HiveWebCrawler"),
